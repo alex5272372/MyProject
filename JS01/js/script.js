c469fb64b969149c0a45f6af5016d96a9467399c
alex5272372 <alex5272372@gmail.com>
2019-02-05 10:57:00 +0200

    JS01

--- JS01/js/script.js
+++ JS01/js/script.js
@@ -4,4 +4,22 @@ while(name === ""){
 }
 if(name != null){
     let age = prompt("Enter your age", "");
+    while(isNaN(age)){
+        age = prompt("Enter correct age", age);
+    }
+    if(age != null){
+        let welcome;
+        if(age < 18){
+            welcome = false;
+        } else if(age > 22){
+            welcome = true;
+        } else {
+            welcome = confirm("Are you sure you want to continue?");
+        }
+        if(welcome){
+            alert("Welcome, " + name);
+        } else {
+            alert("You are not allowed to visit this website.");
+        }
+    }
 }

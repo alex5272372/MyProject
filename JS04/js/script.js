5ea16d98544158f18d776cb50aa64c9544d7f935
alex5272372 <alex5272372@gmail.com>
2019-02-19 12:33:51 +0200

    Homework JS04 with 3 numbers

--- JS04/js/script.js
+++ JS04/js/script.js
@@ -1,12 +1,27 @@
-function askNumber(num) {
+function askNumber(message, num) {
     while (num !== null && (isNaN(num) || num == 0 || num != parseInt(num)))
-        num = prompt('Input number', '');
-    return num;
+        num = prompt(`Input ${message} number`, '1');
+    return +num;
 }
 
-function fibonacci(num) {
-    return Math.abs(num) < 2 ? num : fibonacci(num - Math.sign(num)) + fibonacci(num - Math.sign(num) * 2);
+function askNumbers() {
+    let numbersObject = {first: null, second: null, iterator: null};
+    for (curNum in numbersObject)
+        if ((numbersObject[curNum] = askNumber(curNum)) === null) return {cancel: true};
+    return numbersObject;
 }
 
-let num = askNumber();
-if (num !== null) alert(`${num} fibonacci = ${fibonacci(num)}`);
+function fibonacci(numbersObject, iterator) {
+    if (iterator === 1)
+        return numbersObject.first;
+    else if (iterator === 2)
+        return numbersObject.second;
+    else if (Math.abs(iterator) < 2)
+        return iterator;
+    else
+        return fibonacci(numbersObject, iterator - Math.sign(iterator)) + fibonacci(numbersObject, iterator - Math.sign(iterator) * 2);
+}
+
+let numbersObject = askNumbers();
+if (!numbersObject.cancel)
+    alert(`${numbersObject.iterator} fibonacci = ${fibonacci(numbersObject, numbersObject.iterator)}`);

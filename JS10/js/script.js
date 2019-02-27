dae5471a926b308f87b603d26e82ad34e0a8029b
alex5272372 <alex5272372@gmail.com>
2019-02-27 12:13:57 +0200

    Homework JS10

--- JS10/js/script.js
+++ JS10/js/script.js
@@ -1,11 +1,20 @@
 function deadLineInfo(speedArr, backLogArr, deadLineDate) {
-    let afterHours = 0, beforeDays = 0;
+    let speedSum = speedArr.reduce((accumulator, currentValue) => accumulator + currentValue);
+    let backLogSum = backLogArr.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-    alert(afterHours !== 0 ?
-        `Команде разработчиков придется потратить дополнительно ${afterHours} часов после дедлайна, чтобы выполнить все задачи в беклоге.`
-        : `Все задачи будут успешно выполнены за ${beforeDays} дней до наступления дедлайна!`)
+    let nowDate = new Date();
+    nowDate.setHours(
+        deadLineDate.getHours(),
+        deadLineDate.getMinutes(),
+        deadLineDate.getSeconds(),
+        deadLineDate.getMilliseconds()
+    );
+    while(backLogSum > 0 && nowDate.getTime() < deadLineDate.getTime()) {
+        if(nowDate.getDay() !== 0 && nowDate.getDay() !== 6) backLogSum -= speedSum;
+        nowDate.setTime(nowDate.getTime() + 1000 * 60 * 60 * 24);
+    }
+    alert(backLogSum > 0 ?
+        `Команде разработчиков придется потратить дополнительно ${Math.ceil(backLogSum / speedSum * 8)} часов после дедлайна, чтобы выполнить все задачи в беклоге.`
+        : `Все задачи будут успешно выполнены за ${(deadLineDate.getTime() - nowDate.getTime()) / 1000 / 60 / 60 / 24} дней до наступления дедлайна!`);
 }
-
-let mySpeedArr = [54, 87, 60, 93];
-let myBackLogArr = [128, 200, 46, 540, 38, 160, 220, 853];
-deadLineInfo(mySpeedArr, myBackLogArr, new Date('2019-03-08'));
+deadLineInfo([5, 3], [8, 8, 8, 3], new Date('2019-03-04'));

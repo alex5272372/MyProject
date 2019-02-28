50bf4182bf9f69d44e11d95c6f014fc9aa0f43f2
alex5272372 <alex5272372@gmail.com>
2019-02-28 22:09:27 +0200

    CW20190228

--- classwork/js/script.js
+++ classwork/js/script.js
@@ -1,12 +1,16 @@
-//let elementsByTagName = document.getElementsByTagName('div');
-//let elementsByClassName = document.getElementsByClassName('class-name2');
-//let elementById = document.getElementById('container');
-//let element = document.querySelector('.class-name2');
-//console.dir(element);
+function createSq(color) {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('chess-wrapper');
+    document.body.appendChild(wrapper);
+    const documentFragment = document.createDocumentFragment();
 
-let elementsByClassName = document.getElementsByClassName('training-list');
-for(let i = 0; i < elementsByClassName.length; i++) {
-    if(elementsByClassName[i].innerHTML === 'list-element 5') {
-        elementsByClassName[i].innerHTML = 'qqqqqqqqqqqqqqq';
+    for(let i = 0; i < 64; i++) {
+        let block = document.createElement('div');
+        block.classList.add('classname');
+        block.style.backgroundColor = color;
+        documentFragment.appendChild(block);
     }
+    wrapper.appendChild(documentFragment);
 }
+
+createSq('yellow');

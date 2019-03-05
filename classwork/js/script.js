cbdaed2eec53023fb7898aed7a1b808ae8480544
alex5272372 <alex5272372@gmail.com>
2019-03-05 22:05:24 +0200

    Classwork 20190305

--- classwork/js/script.js
+++ classwork/js/script.js
@@ -1,16 +1,40 @@
-function createSq(color) {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('chess-wrapper');
-    document.body.appendChild(wrapper);
-    const documentFragment = document.createDocumentFragment();
+/*
+//mouseOver.addEventListener('mouseover', () => {alert('asd')});
 
-    for(let i = 0; i < 64; i++) {
-        let block = document.createElement('div');
-        block.classList.add('classname');
-        block.style.backgroundColor = color;
-        documentFragment.appendChild(block);
-    }
-    wrapper.appendChild(documentFragment);
+function getRandomArbitrary(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
 }
 
-createSq('yellow');
+// elem.onclick = () => {
+//     elem.style.backgroundColor = `rgb(${getRandomArbitrary(0,255)},${getRandomArbitrary(0,255)},${getRandomArbitrary(0,255)})`
+// };
+
+const elem = document.getElementsByClassName('change-color-block')[2];
+const button = document.getElementById('changeColorBtn');
+// button.addEventListener('click', () => {
+//     elem.style.backgroundColor = `rgb(${getRandomArbitrary(0,255)},${getRandomArbitrary(0,255)},${getRandomArbitrary(0,255)})`;
+// });
+const input = document.getElementById('inputColor');
+
+button.onclick = () => {
+    elem.style.backgroundColor = input.value;
+};*/
+
+const packMan = document.querySelector('.packman-top');
+document.addEventListener('keydown', (event) => {
+switch (event.keyCode) {
+    case 37:
+        console.log('qq');
+        packMan.offsetLeft.value -= 10;
+        break;
+    case 38:
+        packMan.offsetTop.value -= 10;
+        break;
+    case 39:
+        packMan.offsetLeft.value += 10;
+        break;
+    case 40:
+        packMan.offsetLeft.value += 10;
+        break;
+}
+});

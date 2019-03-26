1472d7066b70623886e3e2245bf4801202765454
alex5272372 <alex5272372@gmail.com>
2019-03-26 17:37:18 +0200

    Step 1 Section 4

--- step1/js/script.js
+++ step1/js/script.js
@@ -1,5 +1,6 @@
 let serviceTab;
 let workTab;
+let workMore = 1;
 
 const displayServiceTab = function () {
     $(`.service-tab:not(:eq(${serviceTab}))`).removeClass('active-service');
@@ -14,7 +15,13 @@ const displayWorkTab = function () {
     if(workTab) {
         $(`.work-img:not(.cat${workTab})`).hide();
         $(`.cat${workTab}`).show();
-    } else $('.work-img').show();
+        $('.work-button').hide();
+    } else {
+        $(`.work-img:lt(${workMore * 12})`).show();
+        $(`.work-img:gt(${workMore * 12 - 1})`).hide();
+        if (workMore === 3) $('.work-button').hide();
+        else $('.work-button').show();
+    }
 };
 
 const ready = function () {
@@ -27,13 +34,55 @@ const ready = function () {
         displayServiceTab();
     });
 
+    $('.loader-boxes').hide();
     workTab = Number(localStorage.getItem('workTab'));
     displayWorkTab();
 
     $('.work-tabs').click(function (event) {
         workTab = $(event.target).index();
         localStorage.setItem('workTab', workTab);
+        workMore = 1;
         displayWorkTab();
+    });
+
+    let workImg = $('.work-img');
+
+    workImg.mouseenter(function (event) {
+        $(event.currentTarget).append($('<div>')
+            .addClass('work-hover')
+            .append($('<div>')
+                .append($('<a href="#">')
+                    .addClass('work-hover-button')
+                    .append($('<i>')
+                        .addClass('fas')
+                        .addClass('fa-link')
+                    )
+                )
+                .append($('<a href="#">')
+                    .addClass('work-hover-button')
+                    .append($('<i>')
+                        .addClass('fas')
+                        .addClass('fa-search')
+                    )
+                )
+            )
+            .append($('<p>creative design</p>').addClass('work-hover-p1'))
+            .append($('<p>Web Design</p>').addClass('work-hover-p2'))
+        );
+    });
+
+    workImg.mouseleave(function (event) {
+        $(event.currentTarget).find('.work-hover').remove();
+    });
+
+    $('.work-button').click(function (event) {
+        event.preventDefault();
+        $('.work .loader-boxes').show();
+        setTimeout(function () {
+            $('.work .loader-boxes').hide();
+            workMore++;
+            displayWorkTab();
+        }, 2000)
     });
 };
 

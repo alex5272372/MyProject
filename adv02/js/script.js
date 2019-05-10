7ec30f43506e8d6c3714fedb590bb819f935a59b
alex5272372 <alex5272372@gmail.com>
2019-05-10 22:01:27 +0300

    CW20190510

---
+++ adv02/js/script.js
@@ -0,0 +1,152 @@
+/**
+ * Класс, объекты которого описывают параметры гамбургера.
+ *
+ * @constructor
+ * @param size        Размер
+ * @param stuffing    Начинка
+ * @throws {HamburgerException}  При неправильном использовании
+ */
+class Hamburger {
+    constructor(size, stuffing) {
+        this._toppings = [];
+        this._size = size;
+        this._stuffing = stuffing;
+    }
+}
+
+/* Размеры, виды начинок и добавок */
+Hamburger.SIZE_SMALL = {
+    price: 50,
+    calories: 20
+};
+Hamburger.SIZE_LARGE = {
+    price: 100,
+    calories: 40
+};
+Hamburger.STUFFING_CHEESE = {
+    price: 10,
+    calories: 20
+};
+Hamburger.STUFFING_SALAD = {
+    price: 20,
+    calories: 5
+};
+Hamburger.STUFFING_POTATO = {
+    price: 15,
+    calories: 10
+};
+Hamburger.TOPPING_MAYO = {
+    name: 'майонез',
+    price: 20,
+    calories: 5
+};
+Hamburger.TOPPING_SPICE = {
+    name: 'приправа',
+    price: 15,
+    calories: 0
+};
+
+/**
+ * Добавить добавку к гамбургеру. Можно добавить несколько
+ * добавок, при условии, что они разные.
+ *
+ * @param topping     Тип добавки
+ * @throws {HamburgerException}  При неправильном использовании
+ */
+Hamburger.prototype.addTopping = function (topping) {
+    try {
+        if (this._toppings.indexOf(topping) === -1) this._toppings.push(topping);
+            else throw new HamburgerException(`Добавка ${topping.name} уже в гамбургере`);
+    } catch (e) {
+        if (e instanceof HamburgerException) console.log(e.name + ': ' + e.message);
+        else throw e;
+    }
+};
+
+/**
+ * Убрать добавку, при условии, что она ранее была
+ * добавлена.
+ *
+ * @param topping   Тип добавки
+ * @throws {HamburgerException}  При неправильном использовании
+ */
+Hamburger.prototype.removeTopping = function (topping) {
+    try {
+        let index = this._toppings.indexOf(topping);
+        if (index !== -1) this._toppings.splice(index, 1);
+            else throw new HamburgerException(`Добавки ${topping.name} нету в гамбургере`);
+    } catch (e) {
+        if (e instanceof HamburgerException) console.log(e.name + ': ' + e.message);
+        else throw e;
+    }
+};
+
+/**
+ * Получить список добавок.
+ *
+ * @return {Array} Массив добавленных добавок, содержит константы
+ *                 Hamburger.TOPPING_*
+ */
+Hamburger.prototype.getToppings = function () {
+    return this._toppings;
+};
+
+/**
+ * Узнать размер гамбургера
+ */
+Hamburger.prototype.getSize = function () {
+    return this._size;
+};
+
+/**
+ * Узнать начинку гамбургера
+ */
+Hamburger.prototype.getStuffing = function () {
+    return this._stuffing;
+};
+
+/**
+ * Узнать цену гамбургера
+ * @return {Number} Цена в тугриках
+ */
+Hamburger.prototype.calculatePrice = function () {
+    return this._toppings.reduce((prevVal, curVal) => prevVal + curVal.price, this._size.price + this._stuffing.price);
+};
+
+/**
+ * Узнать калорийность
+ * @return {Number} Калорийность в калориях
+ */
+Hamburger.prototype.calculateCalories = function () {
+    return this._toppings.reduce((prevVal, curVal) => prevVal + curVal.calories, this._size.calories + this._stuffing.calories);
+};
+
+/**
+ * Представляет информацию об ошибке в ходе работы с гамбургером.
+ * Подробности хранятся в свойстве message.
+ * @constructor
+ */
+function HamburgerException (message) {
+    this.message = message;
+    this.name = "HamburgerError";
+}
+HamburgerException.prototype = Object.create(Error.prototype);
+HamburgerException.prototype.constructor = HamburgerException;
+
+// маленький гамбургер с начинкой из сыра
+var hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+// добавка из майонеза
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+// спросим сколько там калорий
+console.log("Calories: %f", hamburger.calculateCalories());
+// сколько стоит
+console.log("Price: %f", hamburger.calculatePrice());
+// я тут передумал и решил добавить еще приправу
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+// А сколько теперь стоит?
+console.log("Price with sauce: %f", hamburger.calculatePrice());
+// Проверить, большой ли гамбургер?
+console.log("Is hamburger large: %s", hamburger.getSize() === Hamburger.SIZE_LARGE); // -> false
+// Убрать добавку
+hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+console.log("Have %d toppings", hamburger.getToppings().length); // 1
--- classwork/js/script.js
+++ classwork/js/script.js
@@ -1,93 +1,22 @@
-/*
-const Student = {
-    getAverageScore: function () {
-        let sumOfMarks = 0;
-        let numberOfSubjects = 0;
-
-        for(let key in this.tabel) {
-            sumOfMarks += this.tabel[key];
-            numberOfSubjects++;
-        }
-        return sumOfMarks / numberOfSubjects;
-    }
-};
-
-const student1 = {
-  firstName: 'Алина',
-  lastName: 'Коваленко',
-  tabel: {
-      math: 10,
-      history: 11,
-      geography: 3
-  }
-};
-const student2 = {
-  firstName: 'Мария',
-  lastName: 'Чупаенко',
-  tabel: {
-      math: 12,
-      history: 12,
-      geography: 12
-  }
-};
-const student3 = {
-  firstName: 'Антон',
-  lastName: 'Геращенко',
-  tabel: {
-      math: 4,
-      history: 9,
-      geography: 6
-  },
-};
-
-student3.__proto__ = Student
-
-console.log(student3.getAverageScore());
-*/
-
-/*
-const DanITStudent = {
-    checkPass: function () {
-        const passDate = new Date(this._datePass);
-        const currentDate = new Date();
-        return passDate > currentDate;
-    },
-    changePass: function (date) {
-        this._datePass = date;
-    }
-};
-
-const student1 = {
-    firstName: 'Andrew',
-    lastName: 'Jenkins',
-    _datePass: '2019-02-18'
+function User(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
 }
 
-student1.__proto__ = DanITStudent;
-
-const student2 = {
-    firstName: 'Alena',
-    lastName: 'Indesit',
-    _datePass: '2019-10-18'
-};
-
-student2.__proto__ = DanITStudent;
-
-console.log(student1.checkPass());
-console.log(student2.checkPass());
-student1.changePass('2020-03-05');
-console.log(student1.checkPass());
-*/
-
-const Product = function(id, shortName, fullName, price) {
-    this.id = id;
-    this.shortName = shortName;
-    this.fullName = fullName;
-    this.price = price;
-    this.calcStockPrice = function (value) {
-        return this.price * (1 - value / 100);
+function Client(firstName, lastName, budget) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.budget = (value) => {
+        if(arguments.length) budget = value;
+            else return budget;
     }
-};
+}
 
-const lenovo = new Product(129, 'len', 'lenovo', 20000);
-console.log(lenovo.calcStockPrice(10));
+const client1 = new Client('Gogi', 'Doe', 100500);
+console.log(client1.budget());
+
+function sum() {
+    return [].reduce.call(arguments, function(acc, item) {
+        return acc + item;
+    });
+}

1d313b7348b383d10086ee4d64db3a7a36629ca2
alex5272372 <alex5272372@gmail.com>
2019-05-15 17:12:19 +0300

    gameOver in classwork

--- classwork/js/script.js
+++ classwork/js/script.js
@@ -1,17 +1,63 @@
 class Cell {
-    constructor () {
+    constructor() {
+        // '' for empty, 'X' - player X, 'O' - player O
         this.value = '';
     }
     fill(player) {
         this.value = player;
     }
     clear() {
-        this.value = 0;
+        this.value = '';
     }
 }
+
 
 class Board {
-    constructor () {
+    constructor() {
+        this.board = [
+            [new Cell(), new Cell(), new Cell()],
+            [new Cell(), new Cell(), new Cell()],
+            [new Cell(), new Cell(), new Cell()]
+        ];
+        this.currentPlayer = 'X';
+    }
+    showBoard() {
+        return this.board.map((item)=>{
+            return item.map((cell)=>{
+                return cell.value;
+            })
+        })
+    }
+    turn(x,y) {
+        // TODO: rewrite with try catch
+        if(this.board[y][x].value) {
+            return false
+        }
+        if (this.currentPlayer === 'X') {
+            this.board[y][x].fill(this.currentPlayer);
+            this.currentPlayer = 'O';
+        } else {
+            this.board[y][x].fill(this.currentPlayer);
+            this.currentPlayer = 'X';
+        }
+        console.table(this.showBoard());
+    }
 
+    gameOver() {
+        return ['X','O'].some(curV => this.board.some(curS => curS.every(curE => curE.value === curV))
+        || this.board.some((curS, indS) => this.board.every(curE => curE[indS].value === curV))
+        || this.board.every((curE, indE) => this.board[indE][indE].value === curV)
+        || this.board.every((curE, indE) => this.board[indE][2 - indE].value === curV))
     }
 }
+
+const board = new Board();
+
+board.turn(1,1); // X
+board.turn(2,1); // O
+board.turn(0,2); // X
+board.turn(1,0); // O
+board.turn(2,0); // X
+board.turn(1,2); // O
+
+console.log(board.gameOver());
